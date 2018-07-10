9216c05ac95392cb016036ab088c2833d4cee612
Tyler True <t.to.the.square@gmail.com>
2018-07-10 01:51:12 -0400

    Make it cross-platform
    Remove MT2D dependency, cut out broken PNG support, lowercase all files.

--- Hexen.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Image.h"
-
-Image *Image_Create_IndexedHexenGraphic(Image *image, Pixel *palette);
-void Save_HexenPlanarLump(char *PATH, Image *HexenLump);
-void Save_HexenBitmapLump(char *PATH, Image *HexenLump);
--- Pixel.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-typedef struct {
-	unsigned char red, green, blue;
-} Pixel;
